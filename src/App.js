9fbb5ae239f1fd7e0108e32e45561f3b8aedeacb
hasanuzzman-dev <hasanuzzaman.dev@gmail.com>
2022-09-08 16:31:52 +0600

    remove component and processOath component

--- src/App.js
+++ src/App.js
@@ -7,13 +7,10 @@ import Home from './components/Home';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Guideline from './components/user-routes/Guidline';
-import ImportToken from './components/user-routes/ImportToken';
-
 import MyTransactions from './components/user-routes/MyTransactions';
 import PrivateRoute from './components/user-routes/PrivateRoute';
-import ProfileInfo from './components/user-routes/ProfileInfo';
+import ProcessOauth from './components/user-routes/ProcessOauth';
 import SendToken from './components/user-routes/SendToken';
-import Wallet from './components/user-routes/Wallet';
 import UserDashboard from './components/user-routes/UserDashboard';
 
 
@@ -32,14 +29,14 @@ function App() {
 
 
         <Route path='/user' element={<PrivateRoute />}>
-          <Route path='wallet' element={<Wallet />} />
+
           <Route path='home' element={<Home />} />
           <Route path='dashboard' element={<UserDashboard />} />
           <Route path='send-token' element={<SendToken />} />
 
           <Route path='my-transactions' element={<MyTransactions />} />
-          <Route path='profile-info' element={<ProfileInfo />} />
-          <Route path='import-token' element={<ImportToken />} />
+          <Route path='process-discord-oauth' element={<ProcessOauth />} />
+
           <Route path='guideline' element={<Guideline />} />
         </Route>
       </Routes>
---
+++ src/components/user-routes/ProcessOauth.jsx
@@ -0,0 +1,14 @@
+import Base from "../Base";
+
+const ProcessOauth = () => {
+    return (
+        <Base>
+            <div>
+                <h1>This is ProcessOauth page</h1>
+
+            </div>
+        </Base>
+    );
+};
+
+export default ProcessOauth;
